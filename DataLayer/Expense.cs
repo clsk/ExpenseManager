40e50ea7a38a1d1dd899c9ddbe39d974b88ccf1c
Alan Alvarez <aalvarez@aliensoft.net>
2015-09-01 21:44:32 -0400

    Creates basic ASP.NET MVC 4 installation and introduces Expense and Category models

---
+++ DataLayer/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Expense
+    {
+        public int ExpenseId { get; set; }
+        public double Amount { get; set; }
+    }
+}
