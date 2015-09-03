74a62ac1c980aa5cec1f806341c03b8647221ffa
Alan Alvarez <aalvarez@aliensoft.net>
2015-09-03 17:45:00 -0400

    Clean up RAMRepository after WhenRegisteringCategory test is done

--- ExpenseManager.Tests/WhenRegisteringCategory.cs
+++ ExpenseManager.Tests/WhenRegisteringCategory.cs
@@ -21,6 +21,11 @@ namespace ExpenseManager.Tests
     public class WhenRegisteringCategory
     {
         private static readonly string CategoryName = "Gas";
+        [ClassCleanup]
+        public static void cleanup()
+        {
+            RAMRepository.RAMRepository.SharedInstance.Expenses.Clear();
+        }
 
         [TestMethod]
         public void shouldCheckForEmptyName()
